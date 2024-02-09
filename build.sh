3a98eb3c930ad9aab22c4a2f78d8407d4e4fe144
Macdara Twomey <macdara.twomey@gmail.com>
2024-02-09 21:58:36 +0800

    Cleaned up build.sh

--- build.sh
+++ build.sh
@@ -5,87 +5,52 @@
 # build.sh linux [win-path]
 # build.sh msvc
 
-ARGC=$#
+# ------------ Argument handling -------------------------------------------------
+ArgCount=$#
+Root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-OS="win"
-LINUX_OUTPUT_PATH_TYPE="linux-path"
-if [[ $ARGC -gt 2 ]]
-then
-    echo "Error: Too many arguments passed to script"
-    exit 1
-elif [[ $ARGC -ge 1 ]]
-then
-    OS=$1
+OS=$1
+if [[ $ArgCount == 2 ]]; then LinuxOutputPathType=$2; fi
 
-    if [[ $ARGC -eq 2 ]]
-    then
-        LINUX_OUTPUT_PATH_TYPE=$2
-        if [[ $OS == "win" || $OS == "msvc" ]]
-        then
-            # When OS is windows, we always want windows filepaths
-            echo "Error: Can't set output file path when building with windows"
-            exit 1
-        fi
-    fi
-fi
+# ------------ Flags and sources -------------------------------------------------
+Source="$Root/src/app.cpp"
+Include="-I $Root/deps"
+CommonFlags="-DBASE_DEBUG=1"
+Executable="test.exe"
 
-function wsl_to_win() {
-	echo "$1" | sed 's/\/mnt\/c/c:/g'
-}
+ClangFlags="-g -std=c++20"
+#ClangFlags+=" -fsanitize=address"
+ClangFlags+=" -Wall -pedantic-errors -Wno-unused-variable -Wno-gnu-anonymous-struct -Wno-writable-strings"
+ClangFlags+=" -Wno-nested-anon-types -Wno-gnu-zero-variadic-macro-arguments -Wno-missing-braces"
+ClangFlags+=" -fno-strict-aliasing -Wno-unused-function -Wno-language-extension-token"
+#ClangFlags+=" -fdiagnostics-color"    # forces colour even when piping to sed
 
-ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ClFlags="-std:c++latest /Zc:preprocessor -Zc:strictStrings- -W3 -wd5105 -wd4201 -wd4505 -INCREMENTAL:NO -FC -EHs- -nologo -Zi"
 
-SRC="$ROOT/src/app.cpp"
-INCLUDE="-I $ROOT/deps"
-TARGET="test.exe"
-OUT=""
-LIBDIR=""
+# ------------ Boilerplate -------------------------------------------------------
+if [[ $OS == "win" ]];   then Compiler="clang++.exe" Out="-o "; fi
+if [[ $OS == "linux" ]]; then Compiler="clang++"     Out="-o "; fi
+if [[ $OS == "msvc" ]];  then Compiler="cl.exe"      Out="-out:"; fi
 
-FLAGS="-g -DBASE_DEBUG=1 -std=c++20"
-#FLAGS+=" -fsanitize=address"
-FLAGS+=" -Wall -pedantic-errors"
-FLAGS+=" -Wno-unused-variable"
-FLAGS+=" -Wno-gnu-anonymous-struct"
-FLAGS+=" -Wno-nested-anon-types"
-FLAGS+=" -Wno-gnu-zero-variadic-macro-arguments"
-FLAGS+=" -Wno-missing-braces"
-FLAGS+=" -fno-strict-aliasing"
-FLAGS+=" -Wno-unused-function"
-FLAGS+=" -Wno-language-extension-token"
-FLAGS+=" -Wno-writable-strings"
-#FLAGS+=" -fdiagnostics-color"    # forces colour even when piping to sed
+function wsl_to_win() { echo "$1" | sed 's/\/mnt\/c/c:/g' ; }
 
-if [[ $OS == "win" ]]
-then
-    CC="clang++.exe"
-	LIBS="-l opengl32.lib -l kernel32.lib -l user32.lib -l gdi32.lib"
-	SRC=`wsl_to_win "$SRC"`
-	INCLUDE=`wsl_to_win "$INCLUDE"`
-elif [[ $OS == "msvc" ]]
-then
-    CC="cl.exe"
-	LIBS="-link opengl32.lib kernel32.lib user32.lib gdi32.lib"
-	SRC=`wsl_to_win "$SRC"`
-	INCLUDE=`wsl_to_win "$INCLUDE"`
+if [[ $OS == "win" ]] || [[ $OS == "msvc" ]]; then Source=`wsl_to_win "$Source"`; fi
+if [[ $OS == "win" ]] || [[ $OS == "msvc" ]]; then Include=`wsl_to_win "$Include"`; fi
 
-	# wd5105 because the new MSVC preprocessor warns on microsoft header
-	FLAGS="-DBASE_DEBUG=1 -W3 -std:c++latest /Zc:preprocessor -Zc:strictStrings- -wd5105 -wd4201 -wd4505 -INCREMENTAL:NO -FC -EHs- -nologo -Zi"
-	# This needs to be after LIBS (which has the -link flag) for msvc to work
-	TARGET="-out:test.exe"
-elif [[ $OS == "linux" ]]
-then
-    CC="clang++"
-else
-    echo "Error: Invalid OS '$OS'"
-    exit 1
-fi
+if [[ $OS == "win" ]];  then Link="-l opengl32.lib -l kernel32.lib -l user32.lib -l gdi32.lib"; fi
+if [[ $OS == "msvc" ]]; then Link="-link opengl32.lib kernel32.lib user32.lib gdi32.lib"; fi
 
+Flags="$CommonFlags"
+if [[ $OS == "linux" ]] || [[ $OS == "win" ]]; then Flags+=" $ClangFlags"; fi
+if [[ $OS == "msvc" ]]; 					   then Flags+=" $ClFlags"; fi
+
+# ------------ Compile -----------------------------------------------------------
 pushd build > /dev/null
 
-CMD="$CC $SRC $INCLUDE $FLAGS $LIBS $TARGET"
+CMD="$Compiler $Source $Include $Flags $Link $Out$Executable"
 echo "$CMD"
 
-if [[ $LINUX_OUTPUT_PATH_TYPE == "win-path" ]]
+if [[ $LinuxOutputPathType == "win-path" ]]
 then
 	# Fixup error paths so we can jump to error on WSL
 	OUTPUT=`eval "$CMD" 2>&1`
